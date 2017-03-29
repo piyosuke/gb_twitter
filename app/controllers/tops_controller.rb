5951283ade48ee6a18375bcf68b9725022e4aad4
piyosuke <github@s-memo.net>
2017-03-29 17:18:14 +0900

    お試し

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,6 @@
 class TopsController < ApplicationController
   def index
+    @twitter ||= MyTwitter.new
+    @twitter.client.search("グラブル", lang: "ja", result_type: 'recent', count: 30)
   end
 end
