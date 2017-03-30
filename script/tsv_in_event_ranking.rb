3a12811f8e2a94290cffe4874d80b1c1133e3171
piyosuke <github@s-memo.net>
2017-03-30 23:07:43 +0900

    文字コード変換でエラーになるので一旦外す

--- script/tsv_in_event_ranking.rb
+++ script/tsv_in_event_ranking.rb
@@ -5,7 +5,8 @@ EVENT_COUNT = 28
 
 file = [Rails.root, "data", "result", "#{sprintf("%03d", EVENT_COUNT)}.tsv"].join("/")
 
-CSV.open(file, "w:cp932", col_sep: "\t") do |csv|
+#CSV.open(file, "w:cp932", col_sep: "\t") do |csv|
+CSV.open(file, "w", col_sep: "\t") do |csv|
   csv << ["順位", "ユーザーID", "名前", "RANK", "累計討伐数", "累計貢献度"]
   EventRanking.where(event_count: EVENT_COUNT).order(:ranking).each do |event_ranking|
     csv << [
